99f35e4843f5585ceb56d776f6675724ca271b00
Abdulrahman <syamabdo382@gmail.com>
2026-02-16 01:16:57 +0200

    generate assets

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAvatar1
+  /// assets/images/avatar1.svg
+  static const String assetsImagesAvatar1 = "assets/images/avatar1.svg";
+
+  /// Assets for assetsImagesAvatar2
+  /// assets/images/avatar2.svg
+  static const String assetsImagesAvatar2 = "assets/images/avatar2.svg";
+
+  /// Assets for assetsImagesBalanceIcon
+  /// assets/images/balance_icon.svg
+  static const String assetsImagesBalanceIcon = "assets/images/balance_icon.svg";
+
+  /// Assets for assetsImagesCard1
+  /// assets/images/card1.svg
+  static const String assetsImagesCard1 = "assets/images/card1.svg";
+
+  /// Assets for assetsImagesDashboardIcon
+  /// assets/images/dashboard_icon.svg
+  static const String assetsImagesDashboardIcon = "assets/images/dashboard_icon.svg";
+
+  /// Assets for assetsImagesExpensesIcon
+  /// assets/images/expenses_icon.svg
+  static const String assetsImagesExpensesIcon = "assets/images/expenses_icon.svg";
+
+  /// Assets for assetsImagesIncomeIcon
+  /// assets/images/income_icon.svg
+  static const String assetsImagesIncomeIcon = "assets/images/income_icon.svg";
+
+  /// Assets for assetsImagesInvestmentsIcon
+  /// assets/images/investments_icon.svg
+  static const String assetsImagesInvestmentsIcon = "assets/images/investments_icon.svg";
+
+  /// Assets for assetsImagesLogoutIcon
+  /// assets/images/logout_icon.svg
+  static const String assetsImagesLogoutIcon = "assets/images/logout_icon.svg";
+
+  /// Assets for assetsImagesSettingIcon
+  /// assets/images/setting-icon.svg
+  static const String assetsImagesSettingIcon = "assets/images/setting-icon.svg";
+
+  /// Assets for assetsImagesStatisticsIcon
+  /// assets/images/statistics_icon.svg
+  static const String assetsImagesStatisticsIcon = "assets/images/statistics_icon.svg";
+
+  /// Assets for assetsImagesTransactionIcon
+  /// assets/images/transaction_icon.svg
+  static const String assetsImagesTransactionIcon = "assets/images/transaction_icon.svg";
+
+  /// Assets for assetsImagesWalletIcon
+  /// assets/images/wallet-icon.svg
+  static const String assetsImagesWalletIcon = "assets/images/wallet-icon.svg";
+}
+
