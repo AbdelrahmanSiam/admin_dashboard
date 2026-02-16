c0997f881ac1e5d6727eac26be20cca866b80b0d
Abdulrahman <syamabdo382@gmail.com>
2026-02-16 23:02:12 +0200

    fix issues of CustomDrawer

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,4 +1,6 @@
+import 'package:admin_dashboard/models/drawer_item_model.dart';
 import 'package:admin_dashboard/utils/app_images.dart';
+import 'package:admin_dashboard/views/widgets/drawer_item.dart';
 import 'package:admin_dashboard/views/widgets/drawer_items_list_view.dart';
 import 'package:admin_dashboard/views/widgets/user_info_list_tile.dart';
 import 'package:flutter/material.dart';
@@ -6,28 +8,50 @@ import 'package:flutter/material.dart';
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 40),
-            UserInfoListTile(
-              image: Assets.assetsImagesAvatar3,
-              title: "Lekan Okeowo",
-              subTitle: "demo@gmail.com",
+        child: CustomScrollView( // we can not use expanded widget inside CustomScrollView directly but we can use it inside SliverFillRemaining
+          slivers: [
+            SliverToBoxAdapter(child: SizedBox(height: 20)),
+            SliverToBoxAdapter(
+              child: UserInfoListTile(
+                image: Assets.assetsImagesAvatar3,
+                title: "Lekan Okeowo",
+                subTitle: "demo@gmail.com",
+              ),
             ),
-            SizedBox(height: 8),
-            DrawerItemsListView(),
+            SliverToBoxAdapter(child: SizedBox(height: 8)),
+            DrawerItemsListView(), // sliverList does not use SliverToBoxAdapter
+            SliverFillRemaining(
+              hasScrollBody: false, // all draw scroll from CustomScrollView
+              child: Column(
+                children: [
+                  Expanded(child: SizedBox()),
+                  DrawerItem(
+                    drawerItemModel: DrawerItemModel(
+                      title: "Setting System",
+                      image: Assets.assetsImagesSettingIcon,
+                    ),
+                    isActive: false,
+                  ),
+                  DrawerItem(
+                    drawerItemModel: DrawerItemModel(
+                      title: "Logout",
+                      image: Assets.assetsImagesLogoutIcon,
+                    ),
+                    isActive: false,
+                  ),
+                  SizedBox(height: 48),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
