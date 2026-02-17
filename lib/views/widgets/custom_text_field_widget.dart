037ba017a07785d1be7ac22cd9ca6728a82edcc5
Abdulrahman <syamabdo382@gmail.com>
2026-02-17 14:52:14 +0200

    create CustomTextFieldWidget

---
+++ lib/views/widgets/custom_text_field_widget.dart
@@ -0,0 +1,27 @@
+import 'package:admin_dashboard/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFieldWidget extends StatelessWidget {
+  const CustomTextFieldWidget({super.key, required this.text, required this.hintText});
+  final String text , hintText;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(text , style: AppStyles.styleMedium16,),
+        SizedBox(height: 5,),
+        TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Color(0XFFFAFAFA),
+            hintText: hintText,
+            hintStyle: AppStyles.styleRegular16.copyWith(color: Color(0XFFAAAAAA),),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/views/widgets/quick_invoice_section.dart
+++ lib/views/widgets/quick_invoice_section.dart
@@ -1,4 +1,5 @@
 import 'package:admin_dashboard/views/widgets/custom_background_container.dart';
+import 'package:admin_dashboard/views/widgets/custom_text_field_widget.dart';
 import 'package:admin_dashboard/views/widgets/quick_invoice_header.dart';
 import 'package:admin_dashboard/views/widgets/latest_transaction_section.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +17,9 @@ class QuickInvoiceSection extends StatelessWidget {
           QuickInvoiceHeader(),
           SizedBox(height: 16,),
           LatestTransactionSection(),
+          Divider(height: 48,), // gives 24 from top and 24 from bottom
+          // CustomTextFieldWidget(text: "Ahmed",hintText: "Ahmed",),
+      
         ],
       ),
     );
