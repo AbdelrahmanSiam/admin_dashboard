834da51a935eac8dc58c1011a73daeb74ed7d68e
Abdulrahman <syamabdo382@gmail.com>
2026-02-18 03:48:57 +0200

    add TransactionContainer to section

--- lib/views/widgets/transaction_container.dart
+++ lib/views/widgets/transaction_container.dart
@@ -2,8 +2,9 @@ import 'package:admin_dashboard/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class TransactionContainer extends StatelessWidget {
-  const TransactionContainer({super.key, required this.title, required this.subTitle, required this.salary});
+  const TransactionContainer({super.key, required this.title, required this.subTitle, required this.salary, this.color = Colors.green});
   final String title ,subTitle ,salary;
+  final Color color;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -13,8 +14,8 @@ class TransactionContainer extends StatelessWidget {
       ),
       child: ListTile(
         title: Text(title,style: AppStyles.styleSemiBold16,),
-        subtitle: Text(subTitle,style: AppStyles.styleRegular16,),
-        leading: Text(salary,style: AppStyles.styleSemiBold20,),
+        subtitle: Text(subTitle,style: AppStyles.styleRegular16.copyWith(color: Color(0XFFAAAAAA)),),
+        trailing: Text(salary,style: AppStyles.styleSemiBold20.copyWith(color: color),),
       ),
     );
   }
--- lib/views/widgets/transaction_section.dart
+++ lib/views/widgets/transaction_section.dart
@@ -1,4 +1,5 @@
 import 'package:admin_dashboard/utils/app_styles.dart';
+import 'package:admin_dashboard/views/widgets/transaction_container.dart';
 import 'package:admin_dashboard/views/widgets/transaction_header.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,12 @@ class TransactionSection extends StatelessWidget {
         SizedBox(height: 20),
         Text("13 April 2022" , style: AppStyles.styleMedium16.copyWith(color: Color(0XFFAAAAAA),),),
         SizedBox(height: 12),
-        
+        TransactionContainer(title: "Cash Withdrawal", subTitle: "13 Apr, 2022 ", salary: r"$20,129"),
+        SizedBox(height: 10),
+        TransactionContainer(title: "Landing Page project", subTitle: "13 Apr, 2022 at 3:30 PM", salary: r"$2,000"),
+        SizedBox(height: 10),
+        TransactionContainer(title: "Juni Mobile App project", subTitle: "13 Apr, 2022 at 3:30 PM", salary: r"$20,129",color: Colors.red,),
+        SizedBox(height: 10),
       ],
     );
   }
