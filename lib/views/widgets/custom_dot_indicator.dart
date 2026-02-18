097a0c45be6cc9fe1985571605d25658fe227dde
Abdulrahman <syamabdo382@gmail.com>
2026-02-18 02:36:57 +0200

    make animate CustomDotIndicator

--- lib/views/widgets/custom_dot_indicator.dart
+++ lib/views/widgets/custom_dot_indicator.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CustomDotIndicator extends StatelessWidget {
-  const CustomDotIndicator({super.key});
-
+  const CustomDotIndicator({super.key, required this.isActive});
+ final bool isActive;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 8,
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      width: isActive ? 32 : 8,
       height: 8,
       decoration: ShapeDecoration(
-        color: Color(0XFF4EB7F2),
+        color: isActive ? Color(0XFF4EB7F2) : Color(0XFFE7E7E7),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
         )
